52003932d6be4395c2f0b39013c6895672a5928d
Marc-Olivier Doyon <marcohd@gmail.com>
2017-03-03 01:30:03 -0500

    Done 00 01 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(tempF)
+	tempC = (tempF - 32.0) * 5.0/9.0
+end
+
+def ctof(tempC)
+	tempF = tempC * 9.0/5.0 + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+def add(x,y)
+	x + y
+end
+
+def subtract(x,y)
+	x - y
+end
+
+def sum(array)
+	sum = 0
+	array.each { |val| sum += val }
+	return sum
+end
+
+def multiply(array)
+	multiply = 1
+	array.each { |val| multiply *= val}
+	return multiply
+end
+
+def power(x,y)
+	x ** y
+end
+
+def factorial(n)
+	factorial = 1
+	while n > 0 do
+		factorial *= n
+		n -= 1
+	end
+	return factorial
+end
