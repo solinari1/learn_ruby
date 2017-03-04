48dc83c6bc6c45929d268ea4c2df2077c2243485
Marc-Olivier Doyon <marcohd@gmail.com>
2017-03-03 20:39:01 -0500

    Added 03 04

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+
+def echo word
+	word
+end
+
+def shout word
+	word.upcase
+end
+
+def repeat(word,n=2)
+	([word]*n).join ' '
+end
+
+def start_of_word(word,n)
+	word[0..n-1]
+end
+
+def first_word phrase
+	phrase.split.first
+end
+
+def titleize phrase
+	little_words = ["and", "the", "over"]
+	phrase = phrase.split.each { |i| i.capitalize! unless little_words.include? i }
+	phrase.first.capitalize!
+	phrase.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+def translate(s)
+	
+	if s.match(/\A[aeiou]/)
+		s + "ay"
+	elsif (s[0] !~ /[aeiou]/ && s[1] !~ /[aeiou]/)
+		s[2..-1] + s[0..1] + "ay"
+	else
+		s[1..-1] + s[0] + "ay"
+	end
+end
+
+
+
